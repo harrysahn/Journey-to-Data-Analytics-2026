51c4a058520d84d4f956577ded5638d22fc1672e
Harry <45388116+harrysahn@users.noreply.github.com>
2026-06-13 22:38:05 +1000

    Update day-07-exercise-03.sql

--- Phase-1-SQL/day-07-exercise-03.sql
+++ Phase-1-SQL/day-07-exercise-03.sql
@@ -2,7 +2,7 @@
 -- Problem Name: Average Salaries
 -- URL: https://platform.stratascratch.com/coding/9917-average-salaries?code_type=3
 -- Task: Compare each employee's salary with the average salary of the corresponding department.
-Output the department, first name, and salary of employees along with the average salary of that department.
+-- Output the department, first name, and salary of employees along with the average salary of that department.
 -- Difficulty: Easy
 
 WITH dept_avg AS
