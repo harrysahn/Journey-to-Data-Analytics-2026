562425d0a905a0f79cd922e66a5b1382cecd3c2b
Harry <45388116+harrysahn@users.noreply.github.com>
2026-06-13 22:40:14 +1000

    Update day-08-exercise-01.sql

--- Phase-1-SQL/day-08-exercise-01.sql
+++ Phase-1-SQL/day-08-exercise-01.sql
@@ -2,7 +2,6 @@
 -- Problem Name: First Names With Six Letters Ending in 'h'
 -- URL: https://platform.stratascratch.com/coding/9842-find-all-workers-whose-first-name-contains-6-letters-and-also-ends-with-the-letter-h?code_type=1
 -- Task: Find all workers whose first name contains 6 letters and also ends with the letter 'h'. Display all information about the workers in output.
--- Output the department, first name, and salary of employees along with the average salary of that department.
 -- Difficulty: Easy
 
 SELECT * 
